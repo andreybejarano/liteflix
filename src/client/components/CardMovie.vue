3f8aac51ff6fd2389c5296b8763dabc58aefd1e8
Andrey Bejarano <andreybejarano@gmail.com>
2019-02-17 19:30:02 -0300

    feat: create component CardMovie

---
+++ src/client/components/CardMovie.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-img v-if="movie.backdrop_path" :src="getUrlImg()" width="300px" height="200px">
+
+  </v-img>
+</template>
+
+<script>
+import config from "@/config";
+
+export default {
+  props: {
+    movie: {}
+  },
+  methods: {
+    getUrlImg() {
+      return `${config.urlAssets}${this.movie.backdrop_path}`;
+    }
+  }
+}
+</script>
