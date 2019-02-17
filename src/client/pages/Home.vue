902ba6ab2792fb83bdddf0573781bc4afc492684
Andrey Bejarano <andreybejarano@gmail.com>
2019-02-17 19:32:05 -0300

    feat: use components recomended and carousel movies

--- src/client/pages/Home.vue
+++ src/client/pages/Home.vue
@@ -1,3 +1,39 @@
 <template>
-  <h2>Home</h2>
+  <div class="white--text">
+    <lf-recomended-movie :movie="recomendedMovie"></lf-recomended-movie>
+    <div class="upcomming">
+      <h2>Próximamente</h2>
+      <lf-carousel-movies v-if="nowPlaying.length!==0" :movies="upcoming"></lf-carousel-movies>
+    </div>
+    <div class="top-rated">
+      <h2>Mejores ranqueadas</h2>
+      <lf-carousel-movies v-if="topRated.length!==0" :movies="topRated"></lf-carousel-movies>
+    </div>
+    <div class="popular">
+      <h1>POPULARES DE LITEFLIX</h1>
+      <lf-carousel-movies v-if="popular.length!==0" :movies="popular"></lf-carousel-movies>
+    </div>
+  </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+import LfRecomendedMovie from "@/components/RecomendedMovie.vue";
+import LfCarouselMovies from "@/components/CarouselMovies.vue";
+
+export default {
+  components: {
+    LfRecomendedMovie,
+    LfCarouselMovies
+  },
+  computed: {
+    ...mapState({
+      nowPlaying: state => state.movies.nowPlaying,
+      upcoming: state => state.movies.upcoming,
+      topRated: state => state.movies.topRated,
+      popular: state => state.movies.popular
+    }),
+    ...mapGetters(["recomendedMovie"])
+  }
+};
+</script>
