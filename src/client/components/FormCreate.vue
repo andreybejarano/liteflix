3569b11a981015090a1881caf125e919a438e29a
Andrey Bejarano <andreybejarano@gmail.com>
2019-02-18 03:27:10 -0300

    feat:  create Page create movie

---
+++ src/client/components/FormCreate.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-form class="form-create">
+    <v-container>
+      <v-layout row wrap align-center>
+        <v-flex xs12>
+          <v-text-field v-model="form.title" label="Titulo" required></v-text-field>
+        </v-flex>
+        <v-flex xs12>
+          <v-text-field v-model="form.original_title" label="Titulo original" required></v-text-field>
+        </v-flex>
+        <v-flex xs12>
+          <v-text-field v-model="form.overview" label="Visión general" required></v-text-field>
+        </v-flex>
+        <v-flex xs12>
+          <v-text-field v-model="form.backdrop_path" label="URL imagen de fondo" required></v-text-field>
+        </v-flex>
+        <v-flex xs6>
+          <v-radio-group v-model="form.type">
+            <v-radio
+              v-for="(type, key) in typesMovie"
+              :key="key"
+              :label="type.text"
+              :value="type.value"
+            ></v-radio>
+          </v-radio-group>
+        </v-flex>
+        <v-flex xs6>
+          <v-checkbox v-model="form.adults" label="Adultos"></v-checkbox>
+        </v-flex>
+        <v-flex xs12>
+          <v-btn block large color="red" dark @click="save">Crear</v-btn>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        title: "",
+        original_title: "",
+        overview: "",
+        backdrop_path: "",
+        adults: false,
+        type: ""
+      },
+      typesMovie: [
+        { value: "upcoming", text: "Proximas" },
+        { value: "topRated", text: "Mejores ranqueadas" },
+        { value: "popular", text: "Populares" }
+      ]
+    };
+  },
+  methods: {
+    save() {
+      this.$emit("onSave", this.form);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.form-create {
+  margin-top: 75px;
+  background: #ffffff;
+}
+</style>
+
---
+++ src/client/pages/CreateMovie.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-layout row wrap justify-center class="white--text">
+    <lf-form-create @onSave="onSave"></lf-form-create>
+    <v-snackbar
+      class="alert-created"
+      v-model="alert"
+      color="#1db328"
+      multi-line
+      :timeout="2000"
+      vertical
+    >
+      Pelicula guardada
+      <v-btn dark flat @click="alert = false">Cerrar</v-btn>
+    </v-snackbar>
+  </v-layout>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+
+import LfFormCreate from "@/components/FormCreate.vue";
+export default {
+  data() {
+    return {
+      alert: false
+    };
+  },
+  components: {
+    LfFormCreate
+  },
+  methods: {
+    ...mapActions(["saveMovie"]),
+
+    onSave(res) {
+      this.saveMovie(res);
+      this.alert = true;
+      setTimeout(() => {
+        this.$router.push({ name: "home" });
+      }, 1000);
+    }
+  }
+};
+</script>
+
