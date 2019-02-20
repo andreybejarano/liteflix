d977c88d27ff3ded093cf83bcca09da7409c764b
Andrey Bejarano <andreybejarano@gmail.com>
2019-02-20 03:56:44 -0300

    feaT: footer

--- src/client/components/CarouselMovies.vue
+++ src/client/components/CarouselMovies.vue
@@ -1,9 +1,19 @@
 <template>
-  <v-layout v-if="movies.length !== 0" row wrap class="white--text">
-    <div class="container-movie" v-for="(movie, key) in moviesToshow" v-bind:key="key">
-      <lf-card-movie :movie="movie"></lf-card-movie>
+  <v-container>
+    <div>
+      <v-layout v-if="movies.length !== 0" class="white--text">
+        <v-flex
+          xs12
+          md3
+          class="container-movie"
+          v-for="(movie, key) in moviesToshow"
+          v-bind:key="key"
+        >
+          <lf-card-movie :movie="movie"></lf-card-movie>
+        </v-flex>
+      </v-layout>
     </div>
-  </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -12,13 +22,13 @@ export default {
   data() {
     return {
       moviesToshow: []
-    }
+    };
   },
   props: {
     movies: undefined
   },
   created() {
-    this.moviesToshow = this.movies.filter((movie, index) => index < 5)
+    this.moviesToshow = this.movies.filter((movie, index) => index < 5);
   },
   components: {
     LfCardMovie
@@ -27,8 +37,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .container-movie {
-    margin: 0 8px;
-  }
+.container-movie {
+  margin: 0px 8px 0px 0px;
+}
 </style>
 
--- src/client/layout/components/Footer.vue
+++ src/client/layout/components/Footer.vue
@@ -1,5 +1,80 @@
 <template>
   <footer class="white--text">
-    <p>footer</p>
+    <v-container>
+      <v-layout row wrap>
+        <v-flex xs12>
+          <v-layout row wrap>
+            <v-flex xs1>
+              <v-img width="22px" src="/static/img/ic-facebook.svg"/>
+            </v-flex>
+            <v-flex xs1>
+              <v-img width="22px" src="/static/img/ic-instagram.svg"/>
+            </v-flex>
+            <v-flex xs1>
+              <v-img width="22px" src="/static/img/ic-twitter.svg"/>
+            </v-flex>
+            <v-flex xs1>
+              <v-img width="22px" src="/static/img/ic-youtube.svg"/>
+            </v-flex>
+          </v-layout>
+        </v-flex>
+        <v-flex xs12>
+          <v-layout row wrap>
+            <v-flex xs3>
+              <v-list dark>
+                <div class="v-list-tile">
+                  Todos los titulos
+                </div>
+                <div class="v-list-tile">
+                  Prensa
+                </div>
+                <div class="v-list-tile">
+                  Confidencialidad
+                </div>
+                <div class="v-list-tile">
+                  Contacto
+                </div>
+              </v-list>
+            </v-flex>
+            <v-flex xs3>
+              <v-list dark>
+                <div class="v-list-tile">
+                  Cancelar Subscripción
+                </div>
+                <div class="v-list-tile">
+                  Inversores
+                </div>
+                <div class="v-list-tile">
+                  Legales
+                </div>
+              </v-list>
+            </v-flex>
+            <v-flex xs3>
+              <v-list dark>
+                <div class="v-list-tile">
+                  Legales
+                </div>
+                <div class="v-list-tile">
+                  RRHH
+                </div>
+                <div class="v-list-tile">
+                  Cookies
+                </div>
+              </v-list>
+            </v-flex>
+          </v-layout>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </footer>
 </template>
+
+<style lang="scss" scoped>
+.v-list {
+  background: #000000;
+}
+
+.v-list-tile {
+  padding: 8px 0;
+}
+</style>
